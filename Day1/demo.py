d978859c74a20b7e21113fb137d7c51e3b35b0af
Loganathan-7264 <sathishloga7264@gmail.com>
2025-08-12 08:20:16 +0530

    Initial commit

---
+++ Day1/demo.py
@@ -0,0 +1,6 @@
+print("welcome to python")
+
+print(19+3)
+print(13+3)
+
+# this is the sample comment line to understand the code
---
+++ Day1/variables.py
@@ -0,0 +1,21 @@
+full_name = "loganathan"
+city = "pune"
+fav_tool = "Playwright"
+years_experience = 4
+# before swap
+print(full_name,city,fav_tool,years_experience)
+print(full_name)
+print(city)
+print(fav_tool)
+print(years_experience)
+
+#  swapping
+city, fav_tool = fav_tool, city
+
+# after swap
+print(full_name,city,fav_tool,years_experience)
+print(full_name)
+print(city)
+print(fav_tool)
+print(years_experience)
+
