17b2bfafc94f8ea38703190b97245c593d73aba0
Loganathan-7264 <sathishloga7264@gmail.com>
2025-09-01 11:11:12 +0530

    Day5 learning string and it's methods

---
+++ Day5/practice.py
@@ -0,0 +1,42 @@
+# mystr="welcome"
+# print(mystr[:6])
+# # print(mystr[:7])
+# # print(mystr[-5:-3])
+
+# s="hello world"
+# print(s.capitalize())
+# s="HELlo"
+# print(s.casefold())
+# print(s.lower())
+# print(s.upper())
+# s="i am iron man"
+# print(s.title())
+# print(s.swapcase())
+
+# print("Hi".center(4,'*'))
+# s="hello"
+# print(s.find("r"))
+# print(s.index("r"))
+
+# s="banana"
+# print(s.count("a"))
+# s = ("Hello World")
+# print(s.replace("rld",'ooo'))
+
+# s="123Ab"
+# print(s.isalnum())
+# s="abeKJ"
+# print(s.isalpha())
+# s="Aa"
+# print(s.isalnum())
+# s="hello"
+# print(s.islower())
+# s="HEaLL"
+# print(s.isupper())
+
+# s="123"
+# print(s.isdecimal())
+# s="123"
+# print(s.isdigit())
+s="89723"
+print(s.isnumeric())
