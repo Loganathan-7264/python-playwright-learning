085c98caf174fa9caab8f8971bd145e4bec32bb4
Loganathan-7264 <sathishloga7264@gmail.com>
2025-09-01 14:39:05 +0530

    Day6 strings practice

---
+++ Day6/practice.py
@@ -0,0 +1,56 @@
+# # s="123"
+# # print(s.isdecimal())
+# # s="①②"
+# # print(s.isdecimal())
+#
+# # s="123"
+# # print(s.isdigit())
+# # s="①②"
+# # print(s.isdigit())
+# # s="12.4"
+# # print(s.isdigit())
+#
+#
+# # s="123"
+# # print(s.isnumeric())
+# # s="①②"
+# # print(s.isnumeric())
+# # s="12.4"
+# # print(s.isnumeric())
+# # s="⅔"
+# # print(s.isnumeric())
+#
+# # splitting the strings
+#
+# # s=("abc@gmail.com")
+# # lst=s.split("c")
+# # print(lst[0])
+#
+# # s="welcome"
+# # print(s.startswith("we"))
+# # print(s.startswith("WE"))
+# # print(s.endswith("come"))
+# # s="loga.py"
+# # print(s.endswith(".py"))
+#
+# # # trimming spaces
+# # s="    loga "
+# # print(s.strip())
+# # s="     loga    "
+# # print(s.lstrip())
+# # print(s.rstrip())
+#
+# s="hello"
+# # s[0]="H"
+# # s=s[1:]
+# s=s.replace("l","x")
+# print(s)
+# # lst=["l","o","g","a"]
+# # lst[0]="H"
+# # print(lst)
+
+s = "welcome"
+print(s[-5:-2])
+print("hello".find("l"))
+s = "Python"
+print(s[0] + s[-1])
